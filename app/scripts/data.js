9f3ca1b7c0657cf8b6b48c9bbfbcf6d38b071547
Lu Liu <lliu@oanda.com>
2014-03-05 10:45:10 -0500

    Fix some stuff :))

---
+++ app/scripts/data.js
@@ -0,0 +1,10 @@
+trades_data = {
+    "EUR_USD" : {
+        "open_trades" : [
+            { "id" : 12345, "units" : 5, "dir" : "L", "symbol" : "EUR/USD", "time" : 1234567890, "price" : 1.45123, "stop_loss" : 1.2, "take_profit" : 1.7, "trailing_stop" : 10 },
+            { "id" : 12344, "units" : 100, "dir" : "S", "symbol" : "EUR/USD", "time" : 1234567891, "price" : 1.45123, "stop_loss" : 1.2, "take_profit" : 1.7, "trailing_stop" : 10 },
+            { "id" : 789, "units" : 100, "dir" : "S", "symbol" : "EUR/USD", "time" : 1234567891, "price" : 1.45123, "stop_loss" : 1.2, "take_profit" : 1.7, "trailing_stop" : 10 },
+            { "id" : 890, "units" : 100, "dir" : "S", "symbol" : "EUR/USD", "time" : 1234567891, "price" : 1.45123, "stop_loss" : 1.2, "take_profit" : 1.7, "trailing_stop" : 10 }
+        ]
+    }
+}
